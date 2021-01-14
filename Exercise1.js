ac5473b8a081232c2c619efaf608c7c67aabab74
SongJoohyung <49755740+SongJoohyung@users.noreply.github.com>
2021-01-14 08:26:12 -0400

    Create Exercise1.js

---
+++ Exercise1.js
@@ -0,0 +1,56 @@
+// Name: Joo Hyung Song
+// Date: 2021-01-11
+// PURPOSE: In class Exercise #1: (Working with JS syntax fundamentals) 
+
+
+
+
+let x = 4;
+let y = 5.5; // Numeric Try declaration
+
+let z = "class #1"; // String-type declaration
+var name = "John";
+
+console.log(x);
+x = "No ";
+
+let myName = "Mr. M";
+let age = 35;
+let canVote = true  ;
+let invoiceAmount = 89.99;
+let myVariable;
+
+console.log(x);
+
+x = false;
+console.log(x);
+
+let divisionExample = 16 ;
+console.log(divisionExample / 3);
+console.log(divisionExample % 3);
+
+let myFirstIntegerVariable = 4;
+let myFirstFloatVariable = 9.9;
+
+let mySecondInteger = 3;
+mySecondInteger = 5;
+
+console.log("Addition");
+let myAddition = myFirstIntegerVariable + mySecondInteger;
+console.log(myAddition);
+console.log("~~~~~~~~~~~~~~~~~~~~~");
+
+console.log("subtraction");
+let mySubtraction = mySecondInteger - myFirstIntegerVariable;
+console.log(mySubtraction);
+console.log("~~~~~~~~~~~~~~~~~~~~");
+
+console.log("multiplication");
+let myMultiplication = myFirstIntegerVariable * mySecondInteger;
+console.log(myMultiplication);
+console.log("~~~~~~~~~~~~~~~~~~~~");
+
+console.log("Division");
+let myDivision = myMultiplication / 2;
+console.log(myDivision);
+console.log("~~~~~~~~~~~~~~~~~~~~~");
